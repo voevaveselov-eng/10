1b6e1a5622bdbe94118b6494fe25dc623c196c12
Romka <voeva.veselov@gmail.com>
2025-10-20 14:21:06 +0300

    инициализация рабочих файлов

---
+++ test_math_functions.py
@@ -0,0 +1,8 @@
+import math_functions
+
+def test_add(): assert math_functions.add(2, 3) == 5
+def test_subtract(): assert math_functions.subtract(10, 4) == 6
+def test_multiply(): assert math_functions.multiply(3, 7) == 21
+def test_divide(): assert math_functions.divide(20, 4) == 5
+def test_divide_by_zero(): assert math_functions.divide(10, 0) == 0
+#https://github.com/voevaveselov-eng/10.git
